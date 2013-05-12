5ed80404468a236829d6190e16ac3806b4c85049
Egor Kolmogortsev <eburgforever@gmail.com>
2013-05-13 01:46:31 +0600

    updated test code

--- test.erl
+++ test.erl
@@ -3,21 +3,29 @@
 
 test() ->
     pg2:create(lamport_pg),
-    Pid1 = spawn(fun() -> tester(100, 200) end),
-    Pid2 = spawn(fun() -> tester(201, 300) end),
-    Pid3 = spawn(fun() -> tester(301, 400) end),
+    Pid1 = spawn(fun() -> tester(10, 20) end),
+    Pid2 = spawn(fun() -> tester(20, 30) end),
+    Pid3 = spawn(fun() -> tester(30, 40) end),
     {Pid1, Pid2, Pid3}.
 
 tester(Min, Max) ->
     {ok, Pid} = lamport:start_link([self(), lamport_pg]),
     timer:sleep(round(random:uniform() * 1000)),
-    [gen_server:cast(Pid, {cast, Num}) || Num <- lists:seq(Min, Max)],
+    send(Min, Max, Pid),
     loop().
 
+send(N, Max, Pid) when N < Max ->
+    timer:sleep(round(random:uniform() * 100)),
+    gen_server:cast(Pid, {cast, N}),
+    send(N + 1, Max, Pid);
+send(_, _, _) ->
+    ok.
+
+
 loop() -> 
-    loop(undefined).
+    loop([]).
 loop(S) -> 
     receive 
         state -> erlang:display(S), loop(S);
-        A -> erlang:display({self(), A}), loop(A)
+        A -> loop([A | S])
     end. 
